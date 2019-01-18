37e5d45930ef19ef5a330c31d62605df35699621
John Hayes <johnnyhayes7@gmail.com>
2019-01-18 15:20:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == " " ||board[index] == "" ||board[index] == nil
     return false
-  elsif board[index] == "X" || board[index] "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
