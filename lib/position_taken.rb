736c5f4cc16e3364e33b41e3dad3bfd7b6b499df
John Hayes <johnnyhayes7@gmail.com>
2019-01-18 14:31:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, player_move)
   if board[player_move] == " " || "" || nil
     return false
-  elsif board[player_move] == "X" || "O"
+  elsif board[player_move] == "X" || "0"
     return true
   end
 end
